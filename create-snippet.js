49129f7706b22fe2d2391841abd94beaee37dee9
Nick Scialli <7538045+nas5w@users.noreply.github.com>
2020-02-08 11:53:53 -0500

    Add script for creating new snippets

---
+++ create-snippet.js
@@ -0,0 +1,30 @@
+var fs = require("fs");
+
+const split = process.argv[2].split("/");
+let filename = split.pop().trim();
+if (filename.substr(filename.length - 3).toLowerCase() !== ".md") {
+  filename += ".md";
+}
+
+// Directory path
+const dir = "./src/code-snippets/" + split.join("/");
+
+// Full path (directory + filename)
+const path = dir + "/" + filename;
+
+try {
+  // Bail if the file exists already
+  if (fs.existsSync(path)) {
+    throw "File already exists, bailing!";
+  }
+
+  // Create the file with our template
+  fs.mkdir(dir, { recursive: true }, err => {
+    if (err) throw err;
+    fs.createReadStream("./src/md-templates/code-snippet.md").pipe(
+      fs.createWriteStream(path)
+    );
+  });
+} catch (err) {
+  console.error(err);
+}
---
+++ src/md-templates/code-snippet.md
@@ -0,0 +1,40 @@
+---
+title: ""
+linkname: ""
+path: "/"
+category: ""
+description: ""
+ref: ""
+---
+
+<!-- prettier-ignore-start -->
+
+## First, do something
+
+```javascript start
+```
+
+```javascript content
+```
+
+```javascript end
+```
+
+```javascript after
+```
+
+## Then, do something else
+
+```javascript start
+```
+
+```javascript content
+```
+
+```javascript end
+```
+
+```javascript after
+```
+
+<!-- prettier-ignore-end -->
